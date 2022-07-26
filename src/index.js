e122d2d5a9d2b1e30be287e80e55d01d8c2bcb4b
OlesiaShev <106448460+OlesiaShev@users.noreply.github.com>
2022-07-26 22:25:30 +0100

    Links from top to each chapter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ function displaySkills()
 {
     let SkillsElement = document.querySelector("#display-Skills");
     let skillsHTML = "";
-    skillsList = ["About me", "Projects", "Skills", "Sertificates", "Contacts", "CV"];
+    skillsList = ["About", "Projects", "Skills", "Sertificates", "Contacts", "CV"];
     skillsList.forEach(function (skill)
     {
         skillsHTML = skillsHTML +
             `<div class="col-4 col-sm-2">
                 <h6>
-                    ${skill}
+                <a href="#${skill}" class="links">${skill}</a>
                 </h6>
             </div>`       
     });
