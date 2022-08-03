b896eebbb9a1f85f109127cd347d684a3f026156
OlesiaShev <106448460+OlesiaShev@users.noreply.github.com>
2022-08-03 13:43:13 +0100

    Code improving

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 function displaySkills()
 {
     let SkillsElement = document.querySelector("#display-Skills");
-    let skillsHTML = "";
-    skillsList = ["About", "Projects", "Skills", "Sertificates", "Contacts", "CV"];
+    let skillsHTML = `<div class="container">
+            <div class="row">`;
+    skillsList = ["About", "Projects", "Skills", "Certificates", "Contacts", "CV"];
     skillsList.forEach(function (skill)
     {
         skillsHTML = skillsHTML +
-            `<div class="categories">
+            `
+            <div class="col-4 col-md-2">
+            
                 <h6>
                 <a href="#${skill}" class="links">${skill}</a>
                 </h6>
             </div>`       
     });
-    SkillsElement.innerHTML = skillsHTML;
+    SkillsElement.innerHTML = skillsHTML + `</div></div>`;
 }
 displaySkills();
 
